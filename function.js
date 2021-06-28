8b3f8a3941f36420cc07a002a5ce81939d5e4808
nsaikrishna-uk <79172461+nsaikrishna-uk@users.noreply.github.com>
2021-06-28 23:05:43 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,17 @@
+//Javascript on nested object, a function that you pass in the object and get back the value. 
+
+ 
+
+const function = (obj) => {
+    Object.keys(obj).forEach(key => {
+
+    console.log('key: '+ key + ', value: '+obj[key]);
+
+    if (typeof obj[key] === 'object') {
+            function(obj[key])
+        }
+    })
+}
+
+function(Example);
+© 2021 GitHub, Inc.
